bcdf7501475400b270bfa3adfc4bdf7e6113fdbf
Tom Lany <fake_address@tomlany.net.fake>
2013-03-09 00:51:24 -0600

    First pass of changes to the script and readme

--- readme.md
+++ readme.md
@@ -1,58 +1,82 @@
-# Weather Data Grabber
+# Current NOAA Weather Grabber
 
-A basic PHP script that grabs and outputs weather data in JSON for use on other UCF sites.
-Uses a simple caching mechanism that saves previously grabbed results to a text file and
-refers to that text file if another request is made within the set cache duration.
+This lightweight PHP script gets the current weather condition, temperature, and the name of a corresponding condition image from NOAA and makes the data available for use in a PHP document.
 
-The script is currently written to accept and parse NOAA XML data.  Condition codes for images
-are strings by default but are converted to the Weather.com/Yahoo condition code standard
-(since most of our sites are already using it anyway.)
+A built-in caching mechanism saves the results to a JSON file. Requests made within the cache period receive cached data. The cache is updated for the first request after it expires.
 
-Links to various icon sets are provided within the feed; `img/weather-small/` contains icons used
-on ucf.edu; `img/weather-medium/` contains icons used in the GMUCF emails; `img/weather-large/` 
-contains icons used on UCF Today.
+This was designed to handle current weather conditions. It could be expanded to handle full forecasts.
 
-Note that this is written primarily to handle current status conditions, but could be expanded
-relatively easily to handle extended forecasts.
+Web URL: https://github.com/TomLany/Weather-Grabber
+Modified heavily by: Tom Lany, http://tomlany.net/
+Based on: https://github.com/UCF/Weather-Data
 
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-## Returned Values
+## Setup
+You need to save the `weather.php` file on your web server where it can be used by the file you want to include weather data on.
 
-### successfulCache (string)
-Either 'yes' or 'no'; if the requested data was successfully cached, this value is set to 'yes'.
-If the value is ever 'no', fallback weather data is saved to the cache anyway so that empty
-JSON is never returned.
+Next, you'll need change just a few configuration variables to make it work with your setup. Find these variables at the top of the weather.php file.
 
-### provider (string)
-The URL of the weather data provider.
+### `WEATHER_CITY_CODE`
+Specify the 4-letter code for the location that you want to use. Go to [http://weather.gov/](http://weather.gov/) and search for the location you want. On the resulting page, [currently] to the right of the current temperature, you will find "Current conditions at:", followed by the monitoring location and it's four letter code in parenthesis. You should enter this four letter code into `WEATHER_CITY_CODE`. For example, if you want weather data from Central Park in New York City, NY, USA, type `KNYC`.
 
-### condition (string)
-A weather condition phrase. (See http://w1.weather.gov/xml/current_obs/weather.php)
+### `CACHEDATA_FILE_PATH`
+Enter the full file path to the location where you want this script to save it's data. Make sure the script has access to this location (the directory is writable). It's a good idea to save the data outside of the web tree, so that other people can't view the cached data directly on the web. For example, if your username is bubba and you want to store data in a folder called weather, you might type `/home/bubba/weather/`.
 
-### temp (string)
-Temperature reading. (Includes degree symbol)
+### `TIMEZONE`
+Enter your timezone in PHP format. You can see the list of possible timezones by going to [http://php.net/manual/en/timezones.php](http://php.net/manual/en/timezones.php) or by running `DateTimeZone::listIdentifiers();` in PHP 5.2 or later. For example, if you want to use the New York City, NY, USA timezone, enter `America/New_York`.
 
-### imgCode (int)
-A status condition code, based on the status code provided by NOAA.  This code is translated
-into a Weather.com/Yahoo numerical condition code.
+### `WEATHER_CACHE_DURATION`
+Specify how often you want the weather data cache updated in seconds. By default, the script requests data once per hour (every 3600 seconds). NOAA currently refreshes their feed data about fifty minutes after each hour.
 
-### imgSmall, imgMedium, imgLarge (string)
-Links to relevant weather icons.  `img/weather-small/` contains icons used on ucf.edu; 
-`img/weather-medium/` contains icons used in the GMUCF emails; `img/weather-large/` contains icons 
-used on UCF Today.
+Refraining from updating too frequently will keep your website fast and request data from NOAA less. The cache needs to refresh frequently enough so the weather data is current, though. This script may not update at the same time each hour; it only updates as data is requested.
 
-### cachedAt (string)
-The timestamp when this set of weather data was last cached.
+### `WEATHER_URL` and `CACHEDATA_FILE`
+These variables store the full path to the weather data location and cache data file, respectively. You probably won't need to edit them. 
 
-### feedUpdatedAt (string)
-The timestamp when the feed provider (NOAA) last updated their feed content.
+## Using the Data
+Once you have filled in the configuration information, you are about ready to pull weather data!
 
+### Include `weather.php`
+You will need to include the `weather.php` file in the page where you want weather data by typing the following:
+
+	<?php require_once('weather.php'); ?>
+
+### Use the variables
+You're ready to use the data! The weather data is included in four variables. You can echo each one where you want your weather data. You don't have to use each variable, and you can certainly omit some if that suits your project best. Each variable is described in more detail below.
+
+#### `$weather_condition;`
+This indicates the current weather condition, such as "Sunny".
+
+#### `$weather_temp;`
+This indicates the current temperature in Fahrenheit, such as "80". You may want to include the degree sign (which is `&deg;` in HTML), and/or an F following the temperature.
+
+If you'd prefer to use Celsius, you could change `temp_f` to `temp_c` in weather.php on approximately line 111.
+
+#### `$weather_imgCode;`
+This script outputs a weather image code. It does not include weather images. The images NOAA uses (which have corresponding file names) are here: [http://w1.weather.gov/xml/current_obs/weather.php])http://w1.weather.gov/xml/current_obs/weather.php).
+
+#### `$weather_feedUpdatedAt;`
+This indicates the time NOAA's feed says the weather information was updated. Note this is NOT the time the data was cached locally (which is likely to be a more recent time).
+
+### Check the cache
+Once this script is working with the file you want to include weather data in, make sure the cache is working. Find the cache file on your server and ensure that it is not updated every time you update the page the weather is included on, but only as often as the cache is run. It is imperative that the cache is run properly to ensure good performance.
 
 ## Notes
-* Sites using this service are expected to cache results on their end and should cache the returned 
-JSON data regardless of the successfulCache value.  Data should only be requested from this script at 
-a set interval.  Fallback values are set regardless of the successfulCache value to prevent empty 
-results from being returned.  Re-requesting this script will not refresh an unsuccessful cache
-unless it has expired.
-* Note that the NOAA only refreshes their current condition data once an hour, at (roughly) 45 
-minutes past the hour.  The weather grabber script is set to cache data for 15 minutes.
+* If the remote weather data file cannot be found, or if there is a problem with it, the an empty cache file will be saved. This blank file will cause the script to try to find the weather data and cache it next time the script is run. If you are having problems getting this script going, make sure the remote file location in `CACHEDATA_FILE` looks correct.
+
+## Example
+The following is a complete example of what you could put in the page you want to include weather data on to pull the weather information.
+
+	<?php
+	require_once('weather.php');
+	
+	echo $weather_condition;
+	echo $weather_temp;
+	echo $weather_imgCode;
+	echo $weather_feedUpdatedAt;
+	
+	?>
+
+## Questions?
+If you have any questions/issues, feel free to leave a comment in the issue tracker at (https://github.com/TomLany/Weather-Grabber/issues)[https://github.com/TomLany/Weather-Grabber/issues].
--- weather.php
+++ weather.php
@@ -1,74 +1,87 @@
 <?php
-define('SITE_URL', 'http://'.$_SERVER['SERVER_NAME'].$_SERVER['REQUEST_URI']);
-define('SITE_PATH', dirname(__FILE__).'/');
+/**
+ * Current NOAA Weather Grabber
+ *
+ * This lightweight PHP script gets the current weather condition,
+ * temperature, and the name of a corresponding condition image 
+ * from NOAA and makes the data available for use in a PHP document
+ * It includes a built-in JSON cache.
+ *
+ * See readme.md for more information.
+ *
+ * Web URL: https://github.com/TomLany/Weather-Grabber
+ * Modified heavily by: Tom Lany, http://tomlany.net/
+ * Based on: https://github.com/UCF/Weather-Data
+ *
 
-define('CACHEDATA_PATH_CURRENT', SITE_PATH.'data/data-current.txt');
-define('CACHEDATA_PATH_FORECAST', '');
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-define('WEATHER_URL_CURRENT', 'http://w1.weather.gov/xml/current_obs/KORL.xml');
-define('WEATHER_URL_FORECAST', '');
 
-define('WEATHER_URL_TIMEOUT', 8);			// seconds
-define('WEATHER_CACHE_DURATION', 60 * 15); 	// seconds (15 minutes)
-define('CACHE_DELIMITER', '!@!');
+/**
+ * Configuration
+ * Set these variables to match your desired configuration.
+ * More information is available in readme.md.
+ *
+ **/
+
+// Enter the four letter NOAA city code from http://weather.gov/.
+define('WEATHER_CITY_CODE', '');
+
+// Enter the full file path to your cache data folder. Make sure the folder is writable.
+define('CACHEDATA_FILE_PATH', '');
+
+// Enter your timezone code from http://php.net/manual/en/timezones.php.
+define('TIMEZONE', '');
+
+// Enter the cache duration, in seconds.
+define('WEATHER_CACHE_DURATION', 3600);
+
+// You probably won't need to touch these.
+define('WEATHER_URL', 'http://w1.weather.gov/xml/current_obs/'.WEATHER_CITY_CODE.'.xml');
+define('CACHEDATA_FILE', CACHEDATA_FILE_PATH.'weather_data_'.WEATHER_CITY_CODE.'.json');
+
+// End of configuration -- you're done!
+// See readme.md for more information about including this in your script.
 
 
 /**
  * Returns either previously cached data or newly fetched data
- * depending on the TTL of the cached data and whether or not it exists.
+ * depending on whether or not it exists and whether or not the
+ * cache time has expired.
  *
  * @return array
  **/
-function get_weather_data($forecasttype='current') {	
-	switch ($forecasttype) {
-		case 'forecast':
-			$cache_data 	= CACHEDATA_PATH_FORECAST;
-			$weather_url 	= WEATHER_URL_FORECAST;
-			break;
-		case 'current':
-		default:
-			$cache_data 	= CACHEDATA_PATH_CURRENT;
-			$weather_url 	= WEATHER_URL_CURRENT;
-			break;
-	}
-	
+function get_weather_data() {
+
+	date_default_timezone_set(TIMEZONE);
+
 	// Check if cached weather data already exists
-	$cache_data_contents = file_get_contents($cache_data);
-	
-	// The cache time must be within now and the cache duration 
-	// to return cached data:
-	# TODO: refactor to properly handle a weeks' worth of weather data...
-	if ($cache_data_contents) {
-		switch ($forecasttype) {
-			case 'forecast':
-				# TODO: setup vars for a weeks' worth of weather data...
-				break;
-			case 'current':
-			default:
-				list($c_success, $c_provider, $c_cond, $c_temp, $c_imgcode, $c_img_s, $c_img_m, $c_img_l, $c_cachetime, $c_feedtime) = explode(CACHE_DELIMITER, $cache_data_contents);
-				break;
-		}		
-		if ( date('YmdHis', strtotime($c_cachetime)) > date('YmdHis', strtotime('Now -'.WEATHER_CACHE_DURATION.' seconds')) ) {
-			return array(
-				'successfulCache'	=> $c_success,
-				'provider'			=> $c_provider,
-				'condition'			=> $c_cond,
-				'temp'				=> $c_temp,
-				'imgCode'			=> (int)$c_imgcode,
-				'imgSmall'			=> $c_img_s,
-				'imgMedium'			=> $c_img_m,
-				'imgLarge'			=> $c_img_l,
-				'cachedAt'			=> $c_cachetime,
-				'feedUpdatedAt'		=> $c_feedtime, 
-			);
+	if (file_exists(CACHEDATA_FILE)) {
+		$weather_string = file_get_contents(CACHEDATA_FILE) or die('Cache file open failed.');
+
+		// The cache time must be within now and the cache duration
+		// to return cached data:
+		if ($weather_string) {
+
+			// Checks to see if the cache needs to be updated
+			if ( date('YmdHis', filemtime(CACHEDATA_FILE)) > date('YmdHis', strtotime('Now -'.WEATHER_CACHE_DURATION.' seconds')) ) {
+				$weather = (json_decode($weather_string, true));
+				return $weather;
+			}
+			else {
+				return make_new_cachedata();
+			}
+
 		}
 		else {
-			return make_new_cachedata($cache_data, $weather_url);
+			return make_new_cachedata();
 		}
-	} 
-	else {
-		return make_new_cachedata($cache_data, $weather_url);
+
 	}
+	else {
+		return make_new_cachedata();
+	}
+
 }
 
 
@@ -78,197 +91,62 @@ function get_weather_data($forecasttype='current') {
  *
  * @return array
  **/
-function make_new_cachedata($cache_data, $weather_url) {
+function make_new_cachedata() {
+
+	// End this function if the weather feed cannot be found
+	$weather_url_headers = get_headers(WEATHER_URL);
+	if ($weather_url_headers[0] !== "HTTP/1.0 200 OK") {
+		return;
+	}
+
+	// Setup an empty array for weather data
 	$weather = array(
-		'successfulCache'	=> '',
-		'provider'			=> '',
-		'condition'			=> 'Fair', 		// Fallback
-		'temp'				=> '80&#186;',	// Fallback
-		'imgCode'			=> 34,			// Fallback
-		'imgSmall'			=> '',
-		'imgMedium'			=> '',
-		'imgLarge'			=> '',
-		'cachedAt'			=> '',
+		'condition'			=> '',
+		'temp'				=> '',
+		'imgCode'			=> '',
 		'feedUpdatedAt'		=> '',
 	);
-	
+
 	// Set a timeout and grab the weather feed
 	$opts = array('http' => array(
 							'method' => 'GET',
-							'timeout' => WEATHER_URL_TIMEOUT
+							'timeout' => 5		//seconds
 							));
 	$context = stream_context_create($opts);
-	$raw_weather = file_get_contents($weather_url, false, $context);
-	
+	$raw_weather = file_get_contents(WEATHER_URL, false, $context);
+
+	// If the weather feed can be fetched, grab and sanatize the needed information
 	if ($raw_weather) {
 		$xml = simplexml_load_string($raw_weather);
-		
-		$weather['condition'] 	= (string)$xml->weather;
-		$weather['temp']		= number_format((string)$xml->temp_f).'&#186;'; // strip decimal place
-		$weather['imgCode']		= (string)$xml->icon_url_name;
-		
-		// Convert NOAA's weather icon names to standard weather codes
-		// See http://w1.weather.gov/xml/current_obs/weather.php
-		list($weather_img_name, $ext) = explode('.', $weather['imgCode']);
-		switch ($weather_img_name) {
-			case 'bkn':
-				$weather_code = 28; // Mostly Cloudy
-				break;
-			case 'nbkn':
-				$weather_code = 27; // Mostly Cloudy (night)
-				break;
-			case 'skc':
-				$weather_code = 32; // Fair, Clear
-				break;
-			case 'nskc':
-				$weather_code = 31; // Fair, Clear (night)
-				break;
-			case 'few':
-				$weather_code = 34; // Few Clouds
-				break;
-			case 'nfew':
-				$weather_code = 29; // Few Clouds (night)
-				break;
-			case 'sct':
-				$weather_code = 30; // Partly Cloudy
-				break;
-			case 'nsct':
-				$weather_code = 27; // Partly Cloudy (night)
-				break;
-			case 'ovc':
-			case 'novc':
-				$weather_code = 26; // Overcast (day, night)
-				break;
-			case 'fg':
-			case 'nfg':
-				$weather_code = 20; // Foggy
-				break;
-			case 'smoke':
-				$weather_code = 22; // Smoke
-				break;
-			case 'fzra':
-				$weather_code = 8;  // Freezing drizzle
-				break;
-			case 'ip':
-				$weather_code = 18; // Hail
-				break;
-			case 'mix':
-			case 'nmix':
-				$weather_code = 7;  // Mixed snow and sleet (day, night)
-				break;	
-			case 'raip':
-				$weather_code = 35; // Mixed rain and hail
-				break;	
-			case 'rasn':
-			case 'nrasn':
-				$weather_code = 6;  // Mixed rain and sleet
-				break;	
-			case 'shra':
-				$weather_code = 11; // Light Showers
-				break;
-			case 'tsra':
-				$weather_code = 3;  // Severe Thunderstorms
-				break;	
-			case 'ntrsa':
-			case 'hi_ntrsa':
-				$weather_code = 47; // Thunderstorms, Thunderstorm in vicinity (night)
-				break;
-			case 'sn':
-				$weather_code = 16; // Snow
-				break;	
-			case 'nsn':
-				$weather_code = 46; // Snow (night)
-				break;
-			case 'wind':
-			case 'nwind':
-			case 'nsvrtsra':
-				$weather_code = 23; // Windy (and funnel spout/tornado)
-				break;
-			case 'hi_shwrs':
-				$weather_code = 40; // Showers in Vicinity
-				break;
-			case 'hi_nshwrs':
-			case 'nra':
-				$weather_code = 45; // Showers, Showers in Vicinity (night)
-				break;
-			case 'fzrara':
-				$weather_code = 10; // Freezing Rain
-				break;	
-			case 'hi_tsra':
-				$weather_code = 38; // Thunderstorm in vicinity (day)
-				break;	
-			case 'ra1':
-				$weather_code = 9;  // Drizzle 
-				break;
-			case 'ra':
-				$weather_code = 12; // Showers 
-				break;	
-			case 'dust':
-				$weather_code = 19; // Dust
-				break;
-			case 'mist':
-				$weather_code = 21; // Haze
-				break;					
-		}
-		$weather['imgCode'] = $weather_code;
-		
-		// We assume the fetch was a success unless the
-		// condition, temp or imgCode are empty:
-		$weather['successfulCache'] = 'yes';
-		
-		// Catch missing condition
-		if (!is_string($weather['condition']) or !$weather['condition']){
-			$weather['condition'] = 'Fair';
-			$weather['successfulCache'] = 'no';
-		}
-		
-		// Catch missing temp
-		if (!isset($weather['temp']) or !$weather['temp']){
-			$weather['temp'] = '80&#186;';
-			$weather['successfulCache'] = 'no';
-		}
-		
-		// Catch missing imgCode (cid)
-		if (!isset($weather['imgCode']) or !intval($weather['imgCode'])){
-			$weather['imgCode'] = '34';
-			$weather['successfulCache'] = 'no';
-		}
-		
-		// Set other data
-		$weather['provider'] 		= (string)$xml->credit_URL;
-		$weather['imgSmall'] 		= SITE_URL.'img/weather-small/'.$weather['imgCode'].'.png'; 
-		$weather['imgMedium'] 		= SITE_URL.'img/weather-medium/'.$weather['imgCode'].'.png'; 
-		$weather['imgLarge'] 		= SITE_URL.'img/weather-large/WC'.$weather['imgCode'].'.png';  
-		$weather['cachedAt'] 		= date('r');
-		$weather['feedUpdatedAt'] 	= (string)$xml->observation_time_rfc822;
+
+		$weather['condition'] 		= htmlentities((string)$xml->weather);
+		$weather['temp']			= htmlentities(number_format((string)$xml->temp_f)); // strip decimal place
+		$weather['imgCode']			= htmlentities((string)$xml->icon_url_name);
+		$weather['feedUpdatedAt'] 	= htmlentities((string)$xml->observation_time_rfc822);
 	}
-	
-	// Setup a new string for caching:
-	$string = '';
-	foreach ($weather as $key => $val) {
-		$string .= $val.CACHE_DELIMITER;
-	}
-	$string = substr($string, 0, -strlen(CACHE_DELIMITER));
-	
-	// Write the new string of data to the cache file:
-	if (file_exists($cache_data)) {
-		$filehandle = fopen($cache_data, 'w') or die('Cache file open failed.');
-		fwrite($filehandle, $string);
-		fclose($filehandle);
-	}
-	else {
-		$weather['successfulCache'] = 'no';
-	}
-	
-	// Finally, return the newly-grabbed content:
+
+	// Setup a new string for caching
+	$weather_string = json_encode($weather);
+
+	// Write the new string of data to the cache file
+	$filehandle = fopen(CACHEDATA_FILE, 'w') or die('Cache file open failed.');
+	fwrite($filehandle, $weather_string);
+	fclose($filehandle);
+
+	// Return the newly grabbed content
 	return $weather;
 }
 
 
+/**
+ * Grab, sanatize, and put the data in variables.
+ *
+ **/
+$weather_array = get_weather_data();
 
-// This output can be updated later to output results based
-// on GET params (current, forecast...)
-header('Content-Type: application/json');
-print json_encode(get_weather_data('current'));
+$weather_condition 		= htmlentities($weather_array['condition']);
+$weather_temp 			= htmlentities($weather_array['temp']);
+$weather_imgCode 		= htmlentities($weather_array['imgCode']);
+$weather_feedUpdatedAt 	= htmlentities($weather_array['feedUpdatedAt']);
 
 ?>
